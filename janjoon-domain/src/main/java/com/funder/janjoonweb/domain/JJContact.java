8c58b56a874efa7dcaa7398418782e0acccf8449
jaspart <arnaud.jaspart@gmail.com>
2013-12-18 17:52:32 +0000

    Administration: Contact
    git-svn-id: file:///home/arnaud/svn/janjoon/trunk@83 5a6c6dd2-9592-4175-aa9b-afcaefd3dc25

--- janjoon-domain/src/main/java/com/funder/janjoonweb/domain/JJContact.java
+++ janjoon-domain/src/main/java/com/funder/janjoonweb/domain/JJContact.java
@@ -1,20 +1,22 @@
 package com.funder.janjoonweb.domain;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.entity.RooJpaEntity;
 import org.springframework.roo.addon.tostring.RooToString;
-import java.util.HashSet;
-import java.util.Set;
-import javax.persistence.CascadeType;
-import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
 
 @RooJavaBean
 @RooToString
@@ -22,7 +24,7 @@ import javax.persistence.OneToMany;
 public class JJContact extends JJAbstractEntity {
 
     @NotNull
-    @Size(max = 25)
+    @Size(max = 35)
     private String password;
 
     @NotNull
--- janjoon-ui/src/main/java/com/funder/janjoonweb/ui/mb/JJContactBean.java
+++ janjoon-ui/src/main/java/com/funder/janjoonweb/ui/mb/JJContactBean.java
@@ -1,30 +1,80 @@
 package com.funder.janjoonweb.ui.mb;
 
 import java.util.Date;
+import java.util.List;
 
-import com.funder.janjoonweb.domain.JJContact;
 import org.springframework.roo.addon.jsf.managedbean.RooJsfManagedBean;
 import org.springframework.roo.addon.serializable.RooSerializable;
+
+import com.funder.janjoonweb.domain.JJContact;
 
 @RooSerializable
 @RooJsfManagedBean(entity = JJContact.class, beanName = "jJContactBean")
 public class JJContactBean {
 
-	public JJContact createANDpersistJJContact(String firstname, String lastname, String email, String password) {
-		JJContact newJJContact = new JJContact();
-		newJJContact.setPassword(password);
-		newJJContact.setEmail(email);
-		newJJContact.setLdap(email.indexOf("@"));
-		newJJContact.setFirstname(firstname);
-		newJJContact.setLastname(lastname);
-		newJJContact.setDateofbirth(new Date());
-		newJJContact.setEnabled(true);
-		newJJContact.setAccountNonExpired(true);
-		newJJContact.setCredentialsNonExpired(true);
-		newJJContact.setAccountNonLocked(true);
-		jJContactService.saveJJContact(newJJContact);
-		//newJJContact.setName(email.substring(0, email.indexOf("@")););
+	private JJContact contact;
 
-		return newJJContact;
+	private List<JJContact> contactList;
+
+	// @Email(message = "must be a valid email")
+	// private String email;
+
+	private String message;
+
+	public JJContact getContact() {
+		return contact;
 	}
+
+	public void setContact(JJContact contact) {
+		this.contact = contact;
+	}
+
+	public List<JJContact> getContactList() {
+		contactList = jJContactService.getAllJJContact();
+		return contactList;
+	}
+
+	public void setContactList(List<JJContact> contactList) {
+		this.contactList = contactList;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	// public String getEmail() {
+	// return email;
+	// }
+	//
+	// public void setEmail(String email) {
+	// this.email = email;
+	// }
+
+	public void newContact() {
+		message = "New User";
+		contact = new JJContact();
+		contact.setEnabled(true);
+		contact.setCreationDate(new Date());
+		contact.setDescription("Defined as a Contact");
+	}
+
+	public void editContact() {
+		message = "Edit User";
+	}
+
+	public void save() {
+		JJContact tmpcontact = null;
+		tmpcontact = jJContactService.findJJContact(contact.getId());
+		if (tmpcontact == null) {
+			System.out.println("IS a new Contact");
+			jJContactService.saveJJContact(contact);
+		} else {
+			jJContactService.updateJJContact(contact);
+		}
+	}
+
 }
