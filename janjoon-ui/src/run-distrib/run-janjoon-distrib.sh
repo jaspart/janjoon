f18a3f79a2e8c27c3ddf47de1a9d47458eaf97aa
jaspart <arnaud.jaspart@gmail.com>
2014-01-26 08:08:46 +0000

    TECH executable and synchronized start for run-janjoon-distrib.sh
    git-svn-id: file:///home/arnaud/svn/janjoon/trunk@99 5a6c6dd2-9592-4175-aa9b-afcaefd3dc25

--- janjoon-ui/src/run-distrib/run-janjoon-distrib.sh
+++ janjoon-ui/src/run-distrib/run-janjoon-distrib.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-nohup ./run-browser.sh & 
-java -jar jetty-runner-8.1.9.v20130131.jar janjoon-ui-0.2-BUILD_NUMBER.war
+java -jar jetty-runner-8.1.9.v20130131.jar janjoon-ui-0.2-BUILD_NUMBER.war&
+echo $! > /tmp/janjoon.pid
+sleep 15
+nohup ./run-browser.sh &
