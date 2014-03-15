f2d1be69ac6c87784a02ae5314b5893f0e169f2a
jaspart <arnaud.jaspart@gmail.com>
2014-03-15 17:01:40 +0000

    ADM15: fix issue Jetty/SpringSecurity - test from delivery
    git-svn-id: file:///home/arnaud/svn/janjoon/trunk@187 5a6c6dd2-9592-4175-aa9b-afcaefd3dc25

--- janjoon-ui/src/run-distrib/run-janjoon-distrib.bat
+++ janjoon-ui/src/run-distrib/run-janjoon-distrib.bat
@@ -1,3 +1,3 @@
-java -jar jetty-runner-8.1.9.v20130131.jar janjoon-ui-0.2-BUILD_NUMBER.war
-timeout /t 15 /nobreak > NUL
+java -jar jetty-runner-8.1.9.v20130131.jar --config jetty.xml janjoon-ui-0.2-BUILD_NUMBER.war
+timeout /t 35 /nobreak > NUL
 start http://localhost:8080/
