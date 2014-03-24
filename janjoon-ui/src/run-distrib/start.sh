39aea78ab8a4ccd90cec83106a2e0fe5fe9d1231
jaspart <arnaud.jaspart@gmail.com>
2014-03-24 21:35:28 +0000

    TST-18: update database for bug, testcase, ...
    git-svn-id: file:///home/arnaud/svn/janjoon/trunk@201 5a6c6dd2-9592-4175-aa9b-afcaefd3dc25

--- janjoon-ui/src/run-distrib/start.sh
+++ janjoon-ui/src/run-distrib/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-java -jar jetty-runner-9.1.3.v20140225.jar --config jetty.xml janjoon-ui-0.2-BUILD_NUMBER.war &> janjoon.log &
+java -jar jetty-runner-8.1.9.v20130131.jar --config jetty.xml janjoon-ui-0.2-BUILD_NUMBER.war &> janjoon.log &
 echo $! > /tmp/janjoon.pid
 sleep 35
 #nohup gnome-open http://localhost:8080 & 
