1220f478c0716ff751bed0c3900a07f4a669ca63
jaspart <arnaud.jaspart@gmail.com>
2014-05-08 15:06:25 +0000

    ADM-20:Administration:Dans l'Admin, ajouter un onglet pour upload la license (du client vers le serveur) d'un fichier de licence dans le repertoire /license.
    git-svn-id: file:///home/arnaud/svn/janjoon/trunk@275 5a6c6dd2-9592-4175-aa9b-afcaefd3dc25

---
+++ janjoon-ui/src/main/java/com/starit/janjoonweb/ui/mb/LicenseBean.java
@@ -0,0 +1,93 @@
+package com.starit.janjoonweb.ui.mb;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+
+@ManagedBean
+@SessionScoped
+public class LicenseBean implements Serializable {
+	private UploadedFile file;
+
+	public UploadedFile getFile() {
+		return file;
+	}
+
+	public void setFile(UploadedFile file) {
+		this.file = file;
+	}
+
+	public void upload() {
+		if (file != null) {
+
+			System.out.println("Succesful " + file.getFileName()
+					+ " is uploaded.");
+
+		}
+	}
+
+	public void handleFileUpload(FileUploadEvent event) {
+
+		try {
+
+			File targetFolder = null;
+
+			String userHome = System.getProperty("user.home");
+			String classPath = System.getProperty("java.class.path");
+
+			System.out.println(userHome);
+
+			System.out.println(classPath);
+
+			if (classPath.startsWith(userHome)) {
+				String target = userHome + System.getProperty("file.separator")
+						+ "license";
+				File file = new File(target);
+				if (!file.exists()) {
+
+					file.mkdir();
+				}
+				targetFolder = new File(target);
+			} else {
+				targetFolder = new File("../license");
+			}
+
+			InputStream inputStream = event.getFile().getInputstream();
+
+			OutputStream out = new FileOutputStream(new File(targetFolder,
+
+			event.getFile().getFileName()));
+
+			int read = 0;
+
+			byte[] bytes = new byte[1024];
+
+			while ((read = inputStream.read(bytes)) != -1) {
+
+				out.write(bytes, 0, read);
+
+			}
+
+			inputStream.close();
+
+			out.flush();
+
+			out.close();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
+}
